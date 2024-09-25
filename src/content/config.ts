b7a11da80c47390baffaec1cc78ea600b1ebeb01
Juan A. Martinez <jamvius@gmail.com>
2024-09-25 18:42:51 +0200

    Seccion 6

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: "content",
@@ -7,10 +7,25 @@ const blogCollection = defineCollection({
       date: z.date(),
       description: z.string(),
       image: image().refine((img) => img.width < 1200, { message: "Image should be lower 1200px"}),
-      author: z.string(),
-      tags: z.array(z.string()) 
+      // author: z.string(),
+      author: reference('author'),
+      tags: z.array(z.string()),
+      isDraft: z.boolean().default(false)
     })
 })
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    avatar: image().refine((img) => img.width < 1200, { message: "Image should be lower 1200px"}),
+    twitter: z.string(),
+    linkedIn: z.string(),
+    github: z.string(),
+    bio: z.string(),
+    subtitle: z.string(),
+  })
+})
+
 // El nombre de la clave tiene que ser el mismo que el de la carpeta en content
-export const collections = { blog: blogCollection }
+export const collections = { blog: blogCollection, author: authorCollection }
